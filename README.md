da60b63a7b803abc59110ef7b520dab9d403ea5d
Graham Smith <gman98@me.com>
2013-09-28 15:09:07 -0500

    Added support for saving images

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-Sna.py
-======
-
-A Python implementation of the Snapchat API
-
-Special thanks to Thomas Lackner and Adam Caudill for inspiration
+Sna.py======A Python implementation of the Snapchat API*Sna.py* is a Python implementation of the undocumented Snapchat API. Currently *Sna.py* doesn't distinguish between various media types, it is currently set to only save files as JPEG images.**To Do*** Distinguish between videos/images* Video/image upload support* Port to C#	* Deploy to AppHarbor	* Build for Windows PhoneSpecial thanks to [Thomas Lackner](github.com/tlack) and [Adam Caudill](adamcaudill.com) for inspiration.
--- Sna.py
+++ Sna.py
@@ -1,43 +1,86 @@
 from Crypto.Cipher import AES
-import hashlib
+from hashlib import sha256
 import codecs
-import time
+from time import time
 
 import requests
 import json
 
-SECRET = 'iEk21fuwZApXlz93750dmW22pw389dPwOk'
+URL = "https://feelinsonice-hrd.appspot.com/bq"
+SECRET = b'iEk21fuwZApXlz93750dmW22pw389dPwOk'
 STATIC_TOKEN = 'm198sOkJEn37DjqZ32lpRu76xmw288xSQ9'
 HASH_PATTERN = '0001110111101110001111010101111011010001001110011000110001000110'
-AUTH_TOKEN = ''
+BLOB_KEY = 'M02cnQ51Ji97vwT4'
+
+'''
+ID: id
+Snap ID: c_id
+Media Type: m = 0 (pic), 1 (video)
+Sent Snaps: sts
+Opened: ts
+Sender: sn
+Recipient: rp
+Status: st = 1 (sent to you), 2 (sent by you)
+Time: t
+Screenshot Count: c
+'''
 
 def login(usr, pwd):
-    data = {'timestamp':timestamp(), 'req_token': req_token(STATIC_TOKEN, timestamp()), 'username':usr, 'password':pwd }
+    data = {'timestamp':timestamp(), 'req_token': req_token(STATIC_TOKEN), 'username':usr, 'password':pwd }
     req = requests.post("https://feelinsonice.appspot.com/ph/login", data)
-    AUTH_TOKEN = json.loads(req.text)['auth_token']
-    return req.text
+    auth_token = json.loads(req.text)['auth_token']
+    return req
 
-def sync(usr):
-    data = {'timestamp':timestamp(), 'req_token':req_token(AUTH_TOKEN, timestamp()), 'json': '{}', 'username':usr}
+# Returns data identical to the login request
+def sync(usr, token):
+    data = {'timestamp': timestamp(), 'req_token': req_token(token), 'json': '{}', 'username': usr}
     req = requests.post("https://feelinsonice.appspot.com/ph/sync", data)
-    return req.text
+    return req
 
+def get_blob(usr, token, img_id):
+    # Form URL and download encrypted "blob"
+    blob_url = "https://feelinsonice.appspot.com/ph/blob?id={}".format(img_id)
+    blob_url += "&username=" + usr + "&timestamp=" + str(timestamp()) + "&req_token=" + req_token(token)
+    enc_blob = requests.get(blob_url).content
+    # Save decrypted image
+    img = open(img_id + '.jpg', 'wb')
+    img.write(decrypt(enc_blob))
+    img.close()
+    print(decrypt(enc_blob))
+    return decrypt(enc_blob)
+    
+def unread(usr, pwd):
+    auth_token = json.loads(login(usr, pwd).text)['auth_token']
+    feed = json.loads(sync(usr, auth_token).text)
+    for snap in feed['snaps']:
+        if 'rp' not in snap or 'm' in snap:
+            if snap['m'] == 0 and snap['st'] == 1:
+                get_blob(usr, auth_token, snap['id'])
+            
 def timestamp():
-    return int(round(time.time() * 1000))
+    return int(round(time() * 1000))
 
-def req_token(token, time):
-    s1 = SECRET.encode('utf-8') + token.encode('utf-8')
-    s2 = str(time).encode('utf-8') + SECRET.encode('utf-8')
+def req_token(token):
+    return hash256(token, str(timestamp()))
 
-    s3 = hashlib.sha256(s1).hexdigest()
-    print('s3: ', s3)
-    s4 = hashlib.sha256(s2).hexdigest()
-    print('s4: ', s4)
-
+def hash256(var1, var2):
+    h1 = sha256(SECRET + var1.encode('utf-8')).hexdigest()
+    h2 = sha256(var2.encode('utf-8') + SECRET).hexdigest()
     result = ''
-    for i in range(0, len(s3)):
-        result += s4[i] if HASH_PATTERN[i] is "1" else s3[i]
+    for i in range(0, len(h1)):
+        result += h2[i] if HASH_PATTERN[i] is "1" else h1[i]
     return result
 
-def auth_token(req):
-    return json.loads(req)['auth_token']
+def pad(data, bs=16):
+    pc = bs - len(data) % bs
+    return data + (chr(pc) * pc).encode('utf-8')
+
+def decrypt(data):
+    cipher = AES.new(BLOB_KEY, AES.MODE_ECB)
+    return cipher.decrypt(pad(data))
+
+def encrypt(data):
+    cipher = AES.new(BLOB_KEY, AES.MODE_ECB)
+    return cipher.encrypt(data)
+
+
